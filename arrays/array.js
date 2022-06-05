0edb6f678b64f8f5b192a38e8c7141a2fa19e7f7
Andrew Bradt <andrewjbradt@gmail.com>
2022-06-05 10:25:28 -0400

    implement Array class with get, push, pop, and delete methods

---
+++ arrays/array.js
@@ -0,0 +1,36 @@
+class Array {
+  constructor() {
+    this.data = {};
+    this.length = 0;
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(value) {
+    this.data[this.length] = value;
+    this.length++;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const deletedItem = this.data[index];
+    this._shiftItems(index);
+    return deletedItem;
+  }
+
+  _shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
