91d60fa1fa1f1cec29c902a35f2c107d3461a869
Andrew Bradt <andrewjbradt@gmail.com>
2022-06-05 12:21:26 -0400

    implement hashtable with set, get, and keys methods

---
+++ hashtable/hashtable.js
@@ -0,0 +1,64 @@
+console.clear();
+
+class HashTable {
+  constructor (size) {
+    this.data = new Array(size);
+  }
+
+  // arbitary hashing function - returns val between 0 and this.data.length - 1
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    const address = this._hash(key);
+
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+
+    this.data[address].push([key, value]);
+  }
+
+  get(key) {
+    const address = this._hash(key);
+    const bucket = this.data[address];
+
+    if (bucket) {
+      for (const entry of bucket) {
+        if (entry[0] === key) return entry[1];
+      }
+    }
+
+    return undefined;
+  }
+
+  keys() {
+    const keys = [];
+
+    for (const bucket of this.data) {
+      if (bucket) {
+        for (const entry of bucket) {
+          keys.push(entry[0]);
+        }
+      }
+    }
+
+    return keys;
+  }
+
+}
+
+const hash = new HashTable(2);
+
+hash.set('grapes', 50);
+hash.set('apples', 51);
+hash.set('apples', 3);
+hash.set('kiwi', 100);
+
+console.log(hash.keys());
+console.log(hash.data);
