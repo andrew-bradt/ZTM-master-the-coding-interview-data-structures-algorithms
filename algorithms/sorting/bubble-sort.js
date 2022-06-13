008be9e250999502175fd98066687e9840104a4c
Andrew Bradt <andrewjbradt@gmail.com>
2022-06-13 10:46:13 -0400

    add sorting notes and bubble-sort.js

---
+++ algorithms/sorting/bubble-sort.js
@@ -0,0 +1,40 @@
+/* 
+- Procedure:
+  - Compare first two elements of the array, swapping them if the preceding element is higher than the following element
+  - Continue this process until reaching the lastSortedIndex (which is initialized as the index of the last element)
+  - decrement the lastSortedIndex by 1 to ensure the algorithm doesn't repeatedly compare elements that are in the correct position from prev. passthrough
+  - Repeat this procedure from the beginning of the array until lastSortedIndex = 1
+*/
+
+console.clear();
+const array = [-9, 84, 21, 1, 18, 17, 92, 99, 8, 8, 8, 19, 12, -9128, 2, 1, 32, 3];
+
+const bubbleSort = (array) => {
+  if (array.length <= 1) return array;
+
+  let firstIndex = 0;
+  let secondIndex = 1;
+  let lastSortedIndex = array.length;
+  let steps = 0;
+  while (lastSortedIndex !== 1) {
+    steps++;
+    if (secondIndex === lastSortedIndex) {
+      firstIndex = 0;
+      secondIndex = 1;
+      lastSortedIndex--;
+    }
+
+    if (array[firstIndex] > array[secondIndex]) {
+      const temp = array[firstIndex];
+      array[firstIndex] = array[secondIndex];
+      array[secondIndex] = temp;
+    }
+
+    firstIndex++;
+    secondIndex++;
+  }
+  console.log(steps);
+  return array;
+};
+
+console.log(bubbleSort(array));
---
+++ algorithms/sorting/notes.md
@@ -0,0 +1,22 @@
+# Some Types of Sorts 
+## Elementary Sorts
+- Bubble Sort
+- Insertion Sort
+- Selection Sort
+
+## More Complex Algorithms, Often More Efficient Than Elementary Sorts
+- Merge Sort
+- Quick Sort
+
+# Notes About Each Sort
+
+## Bubble Sort
+- Bubbling Up Largest Value using Multiple Passthroughs
+- Simple Implementation, but Unefficient 
+- Time Complexity - B/W - O(n) / O(n^2)
+- Space Complexity - O(1)
+- Procedure:
+  - Compare first two elements of the array, swapping them if the preceding element is higher than the following element
+  - Continue this process until reaching the lastSortedIndex (which is initialized as the index of the last element)
+  - decrement the lastSortedIndex by 1 to ensure the algorithm doesn't repeatedly compare elements that are in the correct position from prev. passthrough
+  - Repeat this procedure from the beginning of the array until lastSortedIndex = 0
